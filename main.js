71d25b473215388f489e6beffdb8f492c5a8df00
[Shaukat Aziz] <[shoukatazizpak@gmail.com]>
2024-03-13 21:13:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,2 @@
+var name = "shoukat";
+console.log("Hello ".concat(name, ", would you like to learn some TypeScript today?"));
---
+++ main.ts
@@ -0,0 +1,3 @@
+let name: string = "shoukat"; 
+
+console.log(`Hello ${name}, would you like to learn some TypeScript today?`); 
